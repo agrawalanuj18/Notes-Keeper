2b89bb054ca965d0702bdc2863ceaa45d3b85991
Anuj Agrawal <agrawalanuj8622@gmail.com>
2023-08-03 17:12:50 +0530

    add

---
+++ build/precache-manifest.54887b4259ce47cc401829fca7985de0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93c4ea978fa7f523a35b853d006c3c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd944ea4a179057518bc",
+    "url": "/static/js/2.dc8daa36.chunk.js"
+  },
+  {
+    "revision": "1632fbda193cf4c16ee8",
+    "url": "/static/js/main.ff3dfa0a.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
